f2444a5e413e909130291b3ba1dc7f6fb4438210
Kevin Oommen <kevinoommenanish@gmail.com>
2024-05-06 15:31:23 +0530

    Add VendorPerfomance metric APIView

--- backend/api/views.py
+++ backend/api/views.py
@@ -28,3 +28,17 @@ class PurchaseOrderListCreateView(generics.ListCreateAPIView):
 class PurchaseOrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = PurchaseOrder.objects.all()
     serializer_class = PurchaseOrderSerializer
+
+class VendorPerformanceMetricsAPIView(generics.RetrieveAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        performance_metrics = {
+            'on_time_delivery_rate': instance.on_time_delivery_rate,
+            'quality_rating_avg': instance.quality_rating_avg,
+            'average_response_time': instance.average_response_time,
+            'fulfillment_rate': instance.fulfillment_rate,
+        }
+        return Response(performance_metrics, status=status.HTTP_200_OK)
